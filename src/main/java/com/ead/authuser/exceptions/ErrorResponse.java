21c85ef84a12a37f6a8106313e4dec1865fac7f4
Patrickaalves <patrickalvessc@gmail.com>
2024-11-12 20:59:02 -0300

    Alterando o nome do arquivo

--- src/main/java/com/ead/authuser/exceptions/ErrorResponse.java
+++ src/main/java/com/ead/authuser/exceptions/ErrorResponse.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ErrorRecordResponse(
+public record ErrorResponse(
         int errorCode,
         String errorMessage,
         Map<String, String> errorsDetails
--- src/main/java/com/ead/authuser/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ead/authuser/exceptions/GlobalExceptionHandler.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorRecordResponse> handleNotFoundException(NotFoundException ex){
-        ErrorRecordResponse errorResponse = new ErrorRecordResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex){
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }

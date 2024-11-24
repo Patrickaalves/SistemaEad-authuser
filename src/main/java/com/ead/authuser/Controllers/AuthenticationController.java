71933befc3ef0c95e35a2543bfcd9a0c20ed59a1
Patrickaalves <patrickalvessc@gmail.com>
2024-11-24 14:07:21 -0300

    Implementando logs na aplicacao

--- src/main/java/com/ead/authuser/Controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/Controllers/AuthenticationController.java
@@ -3,17 +3,19 @@ package com.ead.authuser.Controllers;
 import com.ead.authuser.dto.UserDto;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
     final UserService userService;
 
     public AuthenticationController(UserService userService) {
@@ -33,5 +35,16 @@ public class AuthenticationController {
         }
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userDto));
+    }
+
+    @GetMapping("/logs")
+    public String index(){
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+
+        return "Loggin spring boot";
     }
 }
